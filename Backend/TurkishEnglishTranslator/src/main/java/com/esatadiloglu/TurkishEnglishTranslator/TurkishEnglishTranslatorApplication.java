0bc11b5d98a8745344da277c66a0b25909e9df70
EsatAdiloglu <esatadiloglu16@gmail.com>
2025-07-10 15:45:00 -0400

    added backend

---
+++ Backend/TurkishEnglishTranslator/src/main/java/com/esatadiloglu/TurkishEnglishTranslator/TurkishEnglishTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.esatadiloglu.TurkishEnglishTranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurkishEnglishTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurkishEnglishTranslatorApplication.class, args);
+	}
+
+}
---
+++ Backend/TurkishEnglishTranslator/src/test/java/com/esatadiloglu/TurkishEnglishTranslator/TurkishEnglishTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esatadiloglu.TurkishEnglishTranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurkishEnglishTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
